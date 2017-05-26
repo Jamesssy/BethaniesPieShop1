c9ba858e62301e47f08ff0933991e96da5a9cfc4
James Johansson <shyco_2000@hotmail.com>
2017-05-26 14:44:20 +0200

    added alot

--- src/BethanysPieShop/Controllers/AccountController.cs
+++ src/BethanysPieShop/Controllers/AccountController.cs
@@ -57,7 +57,7 @@ namespace BethanysPieShop.Controllers
             return View(loginViewModel);
         }
 
-
+        [AllowAnonymous]
         public IActionResult Register()
         {
             return View();
@@ -65,6 +65,7 @@ namespace BethanysPieShop.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [AllowAnonymous]
         public async Task<IActionResult> Register(LoginViewModel loginViewModel)
         {
             if (ModelState.IsValid)
